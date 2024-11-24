e7809b5948aef5fefdf03f8f73fe501ecc0c3cf1
core-17 <tsirukbogdan@gmail.com>
2024-11-24 02:55:00 +0000

    Ваше повідомлення про коміт

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-// Отримуємо посилання на "Завдання" і саме меню
 const taskMenuLink = document.getElementById('task-menu-link');
 const taskMenu = document.getElementById('task-menu');
 
-// Додаємо обробник подій для натискання на "Завдання"
+
 taskMenuLink.addEventListener('click', function(event) {
-    event.preventDefault(); // Забороняємо перезавантаження сторінки при кліку
+    event.preventDefault(); 
     if (taskMenu.style.display === 'block') {
-        taskMenu.style.display = 'none'; // Якщо меню відкрите, закриваємо його
+        taskMenu.style.display = 'none'; 
     } else {
-        taskMenu.style.display = 'block'; // Якщо меню закрите, відкриваємо його
+        taskMenu.style.display = 'block'; 
     }
 });
 
-// Закриваємо меню, якщо натиснули в будь-якому іншому місці
+
 window.addEventListener('click', function(event) {
     if (event.target !== taskMenuLink) {
         taskMenu.style.display = 'none';
